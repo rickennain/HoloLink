13b7698f611b108e5e060c667d61019d87ef4435
rickennain <patrickvidican@gmail.com>
2017-04-02 00:07:22 -0700

    changed the flow a little

--- app/src/main/java/lahacks2017/hololink/MainActivity.java
+++ app/src/main/java/lahacks2017/hololink/MainActivity.java
@@ -1,13 +1,8 @@
 package lahacks2017.hololink;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.location.Location;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -16,55 +11,33 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
-
-    private SensorManager sensorManager;
-    TextView tvSpeed;
-
-    // using a thread because we're not allowed to run a network on the main thread of execution
-    Thread thread = new Thread( new Runnable() {
-        @Override
-        public void run(){
-            try{
-                ServerSocket serverSocket = new ServerSocket(4444);
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-                BufferedReader in = new BufferedReader( // this thing would receive data on where to register hazard markers i think
-                        new InputStreamReader(clientSocket.getInputStream()));
-
-            } catch (Exception e) { e.printStackTrace(); }
-        }
-    });
-
-    thread.start(); //also wtf why is this wrong
-
-    public MainActivity() throws IOException {
-    }
+public class MainActivity extends AppCompatActivity{
+    public MainActivity() throws IOException {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Intent intent = new Intent(this, SensorActivity.class);
 
-        tvSpeed = (TextView) findViewById(R.id.textView);
-        sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
-    }
+        // using a thread because we're not allowed to run a network on the main thread of execution
+        Thread thread = new Thread( new Runnable() {
+            @Override
+            public void run(){
+                try{
+                    ServerSocket serverSocket = new ServerSocket(4444);
+                    Socket clientSocket = serverSocket.accept();
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-    @Override
-    public void onAccuracyChanged(Sensor arg0, int arg1) {
-    }
+                    BufferedReader in = new BufferedReader( // this thing would receive data on where to register hazard markers i think
+                            new InputStreamReader(clientSocket.getInputStream()));
+                    startActivity(intent);
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType()==Sensor.TYPE_GYROSCOPE && event.timestamp != 0){
-            // we only care about rotation about the z-axis (forward)
-            float axisZ = event.values[2];
-            tvSpeed.setText("sending " + String.valueOf( axisZ ));
-            out.print( axisZ ); // but now this thing is out of scope because of the thread
-        }
+                } catch (Exception e) { e.printStackTrace(); }
+            }
+        });
+
+        thread.start(); //also wtf why is this wrong
     }
 }
---
+++ app/src/main/java/lahacks2017/hololink/SensorActivity.java
@@ -0,0 +1,47 @@
+package lahacks2017.hololink;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.TextView;
+
+/**
+ * Created by patrick on 4/1/17.
+ */
+
+public class SensorActivity extends Activity implements SensorEventListener {
+    private final SensorManager mSensorManager;
+    private final Sensor mGyroscope;
+
+    TextView tvSpeed;
+
+    public SensorActivity(TextView textView ) {
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        tvSpeed = (TextView) findViewById(R.id.textView);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType()==Sensor.TYPE_GYROSCOPE && event.timestamp != 0){
+            // we only care about rotation about the z-axis (forward)
+            float axisZ = event.values[2];
+            tvSpeed.setText( String.valueOf( axisZ ));
+            // send axisZ to client HoloLens
+        }
+    }
+}
