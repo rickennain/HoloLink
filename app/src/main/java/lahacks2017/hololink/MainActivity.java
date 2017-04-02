a169d999b6a5103e9ea73ccd46e823e3b076eff5
rickennain <patrickvidican@gmail.com>
2017-04-02 03:34:10 -0700

    should work now, just figure out how to connect

--- app/src/main/java/lahacks2017/hololink/MainActivity.java
+++ app/src/main/java/lahacks2017/hololink/MainActivity.java
@@ -4,10 +4,8 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -16,30 +14,11 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-
+    PrintWriter out;
     private SensorManager sensorManager;
-    TextView tvSpeed;
-
-    // using a thread because we're not allowed to run a network on the main thread of execution
-    Thread thread = new Thread( new Runnable() {
-        @Override
-        public void run(){
-            try{
-                ServerSocket serverSocket = new ServerSocket(4444);
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-                BufferedReader in = new BufferedReader( // this thing would receive data on where to register hazard markers i think
-                        new InputStreamReader(clientSocket.getInputStream()));
-
-            } catch (Exception e) { e.printStackTrace(); }
-        }
-    });
-
-    thread.start(); //also wtf why is this wrong
+    TextView tvGyro;
 
     public MainActivity() throws IOException {
     }
@@ -49,9 +28,27 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvSpeed = (TextView) findViewById(R.id.textView);
+        tvGyro = (TextView) findViewById(R.id.textView);
         sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
         sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
+
+        // using a thread because we're not allowed to run a network on the main thread of execution
+        Thread thread = new Thread( new Runnable() {
+            @Override
+            public void run(){
+                try{
+                    ServerSocket serverSocket = new ServerSocket(4444);
+                    Socket clientSocket = serverSocket.accept();
+                    out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    tvGyro.setText(out.toString());
+
+                    BufferedReader in = new BufferedReader( // this thing would receive data on where to register hazard markers i think
+                            new InputStreamReader(clientSocket.getInputStream()));
+
+                } catch (Exception e) { e.printStackTrace(); }
+            }
+        });
+        thread.start();
     }
 
     @Override
@@ -63,8 +60,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (event.sensor.getType()==Sensor.TYPE_GYROSCOPE && event.timestamp != 0){
             // we only care about rotation about the z-axis (forward)
             float axisZ = event.values[2];
-            tvSpeed.setText("sending " + String.valueOf( axisZ ));
-            out.print( axisZ ); // but now this thing is out of scope because of the thread
+            if (out == null){
+                tvGyro.setText("out is null!");
+            }
+            else {
+                tvGyro.setText("sending " + String.valueOf(axisZ));
+                out.print(axisZ); // but now this thing is out of scope because of the thread
+            }
         }
     }
 }
