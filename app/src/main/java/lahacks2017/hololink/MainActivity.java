7fe3f2fe4718e6b4de98118fb517eb36f147d9b5
rickennain <patrickvidican@gmail.com>
2017-04-01 21:40:35 -0700

    tried to add network on a separate thread

--- app/src/main/java/lahacks2017/hololink/MainActivity.java
+++ app/src/main/java/lahacks2017/hololink/MainActivity.java
@@ -10,12 +10,39 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     TextView tvSpeed;
+
+    // using a thread because we're not allowed to run a network on the main thread of execution
+    Thread thread = new Thread( new Runnable() {
+        @Override
+        public void run(){
+            try{
+                ServerSocket serverSocket = new ServerSocket(4444);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                BufferedReader in = new BufferedReader( // this thing would receive data on where to register hazard markers i think
+                        new InputStreamReader(clientSocket.getInputStream()));
+
+            } catch (Exception e) { e.printStackTrace(); }
+        }
+    });
+
+    thread.start(); //also wtf why is this wrong
+
+    public MainActivity() throws IOException {
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +53,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
         sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
     }
+
     @Override
     public void onAccuracyChanged(Sensor arg0, int arg1) {
     }
@@ -35,7 +63,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (event.sensor.getType()==Sensor.TYPE_GYROSCOPE && event.timestamp != 0){
             // we only care about rotation about the z-axis (forward)
             float axisZ = event.values[2];
-            tvSpeed.setText(String.valueOf(axisZ));
-            }
+            tvSpeed.setText("sending " + String.valueOf( axisZ ));
+            out.print( axisZ ); // but now this thing is out of scope because of the thread
         }
     }
+}
