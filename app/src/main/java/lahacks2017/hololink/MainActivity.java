e20828f7702680fbe03edded698917c05df4d5ed
rickennain <patrickvidican@gmail.com>
2017-04-02 01:11:58 -0700

    a little xml business

--- app/src/main/java/lahacks2017/hololink/MainActivity.java
+++ app/src/main/java/lahacks2017/hololink/MainActivity.java
@@ -14,12 +14,11 @@ import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity{
     public MainActivity() throws IOException {}
-
+    final Intent intent = new Intent(this, SensorActivity.class);
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Intent intent = new Intent(this, SensorActivity.class);
 
         // using a thread because we're not allowed to run a network on the main thread of execution
         Thread thread = new Thread( new Runnable() {
@@ -32,11 +31,12 @@ public class MainActivity extends AppCompatActivity{
 
                     BufferedReader in = new BufferedReader( // this thing would receive data on where to register hazard markers i think
                             new InputStreamReader(clientSocket.getInputStream()));
-                    startActivity(intent); //y u no start
+                    //startActivity(intent); //y u no start
 
                 } catch (Exception e) { e.printStackTrace(); }
             }
         });
         thread.start();
+        startActivity(intent);
     }
 }
